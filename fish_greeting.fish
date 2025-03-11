96d7b6e0fad3b6231ad68d1df60e2a6e52065305
IBuildPortals <123713755+IBuildPortals@users.noreply.github.com>
2025-03-11 03:48:28 +0100

    Add files via upload

---
+++ fish_greeting.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+  set_color magenta
+	echo "🐉 용이 되자!"\n #(Let’s become a dragon!)" (A reference to growth and success.)
+  #echo 🕹 "게임 시작! (Game start!)"
+  #echo 🔥 "안녕하세요, $USER! 오늘도 화이팅!" (Hello, $USER! Let’s do our best today!)
+end
